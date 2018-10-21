f67a6cfbbc9f6e7f1bf4607dc2424f7e5a615269
Jakub <jkaminski@wpi.edu>
2018-10-21 18:46:02 -0400

    initial /cmd_vel topic publisher

---
+++ src/simple_topic_publisher.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist 
+
+rospy.init_node('Planner')
+pub = rospy.Publisher('/gazebo/cmd_vel', Twist, queue_size=1)
+rate = rospy.Rate(2)
+motion_command = Twist()
+
+# scale = 0.05
+scale = 0.1
+# motions = [-10:10] + [10:-10]
+# motions = range(-10,1,10) + range(10,-1,-10)
+
+motions = list(range(0, 10, 1)) + list(range(10, -10, -1)) + list(range(-10, 0, 1))
+
+while not rospy.is_shutdown(): 
+  for vel in motions:
+    motion_command.linear.x = -0.7
+    motion_command.angular.z = vel*scale
+    pub.publish(motion_command)
+    rate.sleep()
