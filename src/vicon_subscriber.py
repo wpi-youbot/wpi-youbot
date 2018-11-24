12bcd7b8717173aec8ac7fc1869a0074360bc8ed
Jakub <jkaminski@wpi.edu>
2018-11-23 21:28:03 -0400

    adding missing rosbags

--- src/vicon_subscriber.py
+++ src/vicon_subscriber.py
@@ -17,18 +17,28 @@ def callback(msg):
         x = msg.markers[1].translation.x
         y = msg.markers[1].translation.y
 
-        # [mm] * 1000 *
-        # ddx = (x - callback.x) * 0.001
         ddx = (x - callback.x) * 0.1
         ddy = (y - callback.y) * 0.1
 
         motion_command = Twist()
-        motion_command.linear.x = ddx
-        motion_command.linear.y = ddy
+        # note that axes are swapped to meet the youbot requirements
+        motion_command.linear.x = ddy
+        motion_command.linear.y = ddx
         pub.publish(motion_command)
 
         callback.x = msg.markers[1].translation.x
         callback.y = msg.markers[1].translation.y
+
+        if ddx > callback.ddx_extr:
+            callback.ddx_extr = ddx
+            print("ddx Extreme is: %f" % (ddx))
+            print("%d. %s appears %d times." % (i, key, wordBank[key]))
+
+
+        if ddy > callback.ddy_extr:
+            callback.ddy_extr = ddy
+
+
 
     else:
         callback.x = msg.markers[1].translation.x
@@ -37,6 +47,8 @@ def callback(msg):
 
 callback.counter = 0
 callback.x = 0
+callback.ddx_extr = 0
+callback.ddy_extr = 0
 callback.y = 0
 callback.timestamp = 0
 
