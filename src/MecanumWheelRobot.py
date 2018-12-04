b99807e9361c572b15347a50c68b591238dc45a0
Calvin He <calvinheworld@gmail.com>
2018-12-03 19:00:02 -0500

    fixed state dimensions

--- src/MecanumWheelRobot.py
+++ src/MecanumWheelRobot.py
@@ -3,14 +3,14 @@ from scipy import integrate
 import matplotlib.pyplot as plt
 import math
 
-# Define KUKA YouBot properties
-m0 = 20  # mass of platform (kg)
-m = 20  # mass of platform (kg)
-m1 = 1.40  # TODO find mass of wheels
-R = 0.100/2  # radius of wheels (m) #
-l = 0.150  # platform center to wheel center (m)
-rho1 = 0.2355  # platform center to back axel (m)
-rho2 = 0.2355  # platform center to front axel (m)
+# Define 0KUKA YouBot properties
+m0 = 20         # mass of platform (kg)
+m = 20          # mass of platform (kg)
+m1 = 1.40       # TODO find mass of wheels
+R = 0.100/2     # radius of wheels (m) #
+l = 0.150       # platform center to wheel center (m)
+rho1 = 0.2355   # platform center to back axel (m)
+rho2 = 0.2355   # platform center to front axel (m)
 
 # Mass moments of inertia
 J0 = (m0 / 12) * (4 * math.pow(l, 2) + math.pow(rho1 + rho2, 2))
@@ -29,13 +29,13 @@ J = J0 + 4 * J2 + 2 * m1 * (math.pow(rho1, 2) + math.pow(rho2, 2) + 2 * math.pow
 
 # Ode function for 4-mecanum-wheeled mobile platform
 def ode_mecanum_mobile_dynamics(t, x):
-    dx = np.zeros((6, 1))
+    dx = np.zeros((6,))
     dx[0:3] = x[3:6]
 
     psi = x[2]
     x_c_dot = x[3]
     y_c_dot = x[4]
-    psi_dot = [5]
+    psi_dot = x[5]
 
     moment_val = 1
     M1 = moment_val
@@ -44,18 +44,24 @@ def ode_mecanum_mobile_dynamics(t, x):
     M4 = moment_val
 
     # TODO need to use driving moments M(1-4) as inputs
-    dx[3] = ((-4 * J1) / A) * (-1) * y_c_dot * psi_dot + (R / A) * (
+    x_c_double_dot = ((-4 * J1) / A) * (-1) * y_c_dot * psi_dot + (R / A) * (
                 (M1 + M2 + M3 + M4) * math.cos(psi) + (M1 - M2 - M3 + M4) * math.sin(psi))
-    dx[4] = ((-4 * J1) / A) * (-1) * x_c_dot * psi_dot + (R / A) * (
+    y_c_double_dot = ((-4 * J1) / A) * (-1) * x_c_dot * psi_dot + (R / A) * (
                 (M1 + M2 + M3 + M4) * math.sin(psi) - (M1 - M2 - M3 + M4) * math.sin(psi))
-    dx[5] = (1 / (J * R)) * [l * (M2 - M1 + M4 - M3) + rho2 * (M2 - M1) + rho1(M4 - M3)]
-
+    psi_double_dot = (1 / (J * R)) * (l * (M2 - M1 + M4 - M3) + rho2*(M2 - M1) + rho1*(M4 - M3))
+    
+    dx[3] = x_c_double_dot
+    dx[4] = y_c_double_dot
+    dx[5] = psi_double_dot
+    
+    #print "xdd = ", x_c_double_dot
+    print "ydd = ", y_c_double_dot
     return dx
 
 
 # Main method
 if __name__ == "__main__":
-    t0, tf = 0, 50  # start and end time
+    t0, tf = 0, 25  # start and end time
     t = np.linspace(t0, tf, 100)  # the points of evaluation of solution
 
     # TODO Define start state
@@ -73,7 +79,12 @@ if __name__ == "__main__":
         x[i, :] = r.integrate(t[i])  # get one more value, add it to the array
         if not r.successful():
             raise RuntimeError("Could not integrate")
-    xd = x[3, :]
+            
+    print "X:", x
+    print "X shape:", x.shape
+    xd = x[:,3]
+    yd = x[:,4]
+    psid = x[:,5]
     plt.plot(t, xd)
 
-    plt.show()
+    plt.show()  
